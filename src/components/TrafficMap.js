f4358fa4f0f49866176172b1b010a77ec1fa726c
Dmitriy Burenok <dmitriyburenok@gmail.com>
2023-12-20 14:09:10 -0800

    improve RGB calculation, add column size slider

--- src/components/TrafficMap.js
+++ src/components/TrafficMap.js
@@ -1,25 +1,49 @@
+import { useState } from "react";
 import Map from "react-map-gl";
 import DeckGL from "@deck.gl/react";
 import { ColumnLayer } from "@deck.gl/layers";
 import { map, values } from "lodash";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
 const initialViewState = getInitialViewState();
 
 function TrafficMap({ props }) {
   const { mapData } = props;
+  const [columnRadius, setColumnRadius] = useState(100);
 
-  const layer = getColumnIntersectionLayer(mapData);
+  const layer = getColumnIntersectionLayer(mapData, columnRadius);
 
   return (
-    <DeckGL
-      style={{ position: "relative" }}
-      height={500}
-      initialViewState={initialViewState}
-      controller={true}
-      layers={[layer]}
-    >
-      <Map reuseMaps={true} attributionControl={false} antialias={true} mapStyle="mapbox://styles/mapbox/streets-v9" />
-    </DeckGL>
+    <>
+      <DeckGL
+        style={{ position: "relative" }}
+        height={500}
+        initialViewState={initialViewState}
+        controller={true}
+        layers={[layer]}
+      >
+        <Map
+          reuseMaps={true}
+          attributionControl={false}
+          antialias={true}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+      </DeckGL>
+      <Box sx={{ width: 600 }}>
+        <Slider
+          aria-label="column-radius"
+          valueLabelDisplay="auto"
+          defaultValue={10}
+          step={2}
+          min={6}
+          max={14}
+          marks={true}
+          scale={calculateRepresentationValue}
+          onChange={(_, newValue) => handleSliderChange(newValue, setColumnRadius)}
+        />
+      </Box>
+    </>
   );
 }
 
@@ -35,18 +59,50 @@ function getInitialViewState() {
   };
 }
 
-function getColumnIntersectionLayer(chartDataSnapshot) {
+function getColumnIntersectionLayer(chartDataSnapshot, columnRadius) {
   return new ColumnLayer({
     data: map(values(chartDataSnapshot), ({ location, trafficLoad }) => ({
       location: [parseFloat(location.long), parseFloat(location.lat)],
       trafficLoad,
     })),
     diskResolution: 15,
-    radius: 100,
+    radius: columnRadius,
     extruded: true,
     elevationScale: 4000,
-    getPosition: (d) => d.location,
-    getElevation: (d) => d.trafficLoad,
-    getFillColor: (d) => [d.trafficLoad * 255, (1 - d.trafficLoad) * 255, 0],
+    getPosition: ({ location }) => location,
+    getElevation: ({ trafficLoad }) => trafficLoad,
+    getFillColor,
   });
+}
+
+function getFillColor({ trafficLoad }) {
+  if (trafficLoad < 0.5) {
+    return [trafficLoad * 2 * 255, 255, 0];
+  }
+
+  return [255, (1 - (trafficLoad * 2 - 1)) * 255, 0];
+}
+
+function calculateRepresentationValue(value) {
+  if (value === 6) {
+    return "Wiry";
+  }
+
+  if (value === 8) {
+    return "Slim";
+  }
+
+  if (value === 10) {
+    return "Standard";
+  }
+
+  if (value === 12) {
+    return "Thick";
+  }
+
+  return "Chonky";
+}
+
+function handleSliderChange(newValue, setColumnRadius) {
+  setColumnRadius(newValue * newValue);
 }
